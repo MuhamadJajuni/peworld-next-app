1a4817515b8264b5f32886fb216f7e6bda418b34
Muh Jajuni <muhamadjajuni1@gmail.com>
2023-12-05 14:13:05 +0700

    first commit

--- src/app/lib/init.ts
+++ src/app/lib/init.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByEPFG5RqrcCFVGPW1iu0LpCRRLcS1GeU",
-  authDomain: "nextjs-14-app-peworld.firebaseapp.com",
-  projectId: "nextjs-14-app-peworld",
-  storageBucket: "nextjs-14-app-peworld.appspot.com",
-  messagingSenderId: "177965426453",
-  appId: "1:177965426453:web:4ac44800503b0c3bb66b1c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app
