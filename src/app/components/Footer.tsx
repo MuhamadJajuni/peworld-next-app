fe6e7ffdbbb5dc02aabe8478643e8c84f3642058
Muh Jajuni <muhamadjajuni1@gmail.com>
2023-12-09 20:38:54 +0700

    feature: +performance improvement

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function FooterLayout() {
   return (
-    <footer className="flex flex-col mt-8 w-full max-h-max absolute bg-[#5E50A1] font-openSansl">
-      <section className="grid grid-cols-1 md:grid-cols-3 w-full mb-0 mx-4 md:mx-10 my-8">
+    <footer className="flex flex-col mt-8 max-w-fit max-h-max absolute bg-[#5E50A1] font-openSansl">
+      <section className="grid grid-cols-1 md:grid-cols-3 max-w-fit mb-0 mx-4 md:mx-10 my-8">
         <div className="flex flex-col">
           <Image src={logoFooter} alt="logo" />
           <p className="text-start text-white my-5">
